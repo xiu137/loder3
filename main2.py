daa62334fe47cdc1e5cc56cf50ec0f19b5069801
xiu137 <1919414254@qq.com>
2024-03-28 20:12:19 +0800

    save output_img

--- main2.py
+++ main2.py
@@ -112,8 +112,6 @@ def screen_boxes(boxes, scale):
  
 def storm_detect(input_img: str, model_path: str, size: int, stride: int,confidence:float=0.5):
     model = YOLO(model_path)
-    if not os.path.exists("./temp/input"):
-        os.makedirs("./temp/input")
     all_files = cut_images(input_img, output_url="./temp/input/", size=size, stride=stride)
     detect_result = []
     for file_keypoint in all_files.keys():
@@ -174,13 +172,13 @@ def clean_temp():
         os.remove(f"./temp/input/{file}")
     os.rmdir("./temp/input")
 
-def main(input_img: str, output_img: str, model_path: str, size_stride: list[tuple],confidence:float=0.5,exclude:float=0.5):
+def main(input_img: str, output_img: str,output_label: str, model_path: str, size_stride: list[tuple],confidence:float=0.5,exclude:float=0.5):
     all_result = []
     for size,stride in size_stride:
         all_result += storm_detect(input_img, model_path, size, stride,confidence)
         # clean_temp()
     fixed_detect_result = remove_overlaps(all_result,exclude)
-    # display_result(input_img, output_img, fixed_detect_result)
+    display_result(input_img, output_img, fixed_detect_result)
     fixed_detect_result = transform_CRS(fixed_detect_result,input_img)
     save_result(fixed_detect_result)
 
